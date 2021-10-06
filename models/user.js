b279e4791e5ed53f76d70a99c7378dd5a4077d11
daniel-aracquine <sarthakmanoch1103@gmail.com>
2021-10-06 21:05:10 +0530

    Fixed invalid email issue

--- models/user.js
+++ models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const validator = require('validator')
 
 const UserSchema = new mongoose.Schema({
   fname: {
@@ -17,7 +18,12 @@ const UserSchema = new mongoose.Schema({
   email: {
     type: String,
     required: true,
-    unique: true
+    unique: true,
+    validate(value) {
+        if(!validator.isEmail(value)){
+            throw new Error('Email is invalid')
+        }
+    }
   },
   hash: {
     type: String,
